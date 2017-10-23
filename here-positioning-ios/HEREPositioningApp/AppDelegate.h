6b71cc70e686bcbb1c845b2eeafce1a9dd764df3
Mikko Honkanen <mihonka@users.noreply.github.com>
2017-10-23 14:52:17 +0300

    Add here-positioning-ios application

---
+++ here-positioning-ios/HEREPositioningApp/AppDelegate.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2016-2017 HERE Europe B.V.
+ * All rights reserved.
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ here-positioning-ios/HEREPositioningApp/AppDelegate.m
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016-2017 HERE Europe B.V.
+ * All rights reserved.
+ */
+
+
+#import "AppDelegate.h"
+#import <NMAKit/NMAKit.h>
+
+// To obtain the application credentials, please register at https://developer.here.com/develop/mobile-sdks
+NSString* const kSampleAppID = @"{YOUR_APP_ID}";
+NSString* const kSampleAppCode = @"{YOUR_APP_CODE}";
+NSString* const kSampleMapLicenseKey = @"{YOUR_LICENSE_KEY}";
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //set application credentials
+    [NMAApplicationContext setAppId:kSampleAppID
+                            appCode:kSampleAppCode
+                         licenseKey:kSampleMapLicenseKey];
+
+    return YES;
+}
+
+@end
