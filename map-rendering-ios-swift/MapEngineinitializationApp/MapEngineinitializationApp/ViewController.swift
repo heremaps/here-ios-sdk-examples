daa492e2fae9b91718654ccd75d2339a539d3fc7
Liashko, Oleksandr <ext-oleksandr.liashko@here.com>
2018-07-18 13:28:33 +0300

    Swift example respective to map-rendering-ios objective-c example.

---
+++ map-rendering-ios-swift/MapEngineinitializationApp/MapEngineinitializationApp/ViewController.swift
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2011-2018 HERE Europe B.V.
+ * All rights reserved.
+ */
+
+import UIKit
+import NMAKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: NMAMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //create geo coordinate
+        let geoCoordCenter = NMAGeoCoordinates(latitude: 49.260327, longitude: -123.115025)
+        //set map view with geo center
+        self.mapView.set(geoCenter: geoCoordCenter, animation: .none)
+        //set zoom level
+        self.mapView.zoomLevel = 13.2;
+    }
+}
