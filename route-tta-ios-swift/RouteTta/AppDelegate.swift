83c8b7ab00b79b5585a52401b93e6001f0c7b4e4
dashchak <ext-roman.dashchakivskyi@here.com>
2019-06-27 23:11:53 +0300

    Cleanup, fixed warnings

--- route-tta-ios-swift/RouteTta/AppDelegate.swift
+++ route-tta-ios-swift/RouteTta/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
 import NMAKit
 
 // To obtain the application credentials, please register at https://developer.here.com/develop/mobile-sdks
-let keys = (
+let credentials = (
     appId: "",
     appCode: "",
     licenseKey: ""
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        NMAApplicationContext.setAppId(keys.appId, appCode: keys.appCode, licenseKey: keys.licenseKey)
+        NMAApplicationContext.setAppId(credentials.appId, appCode: credentials.appCode, licenseKey: credentials.licenseKey)
         return true
     }
 }
