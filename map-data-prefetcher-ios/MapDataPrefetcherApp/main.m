43fb1e5a8a07d686c781c435240e8ea7372f0244
Dmitry <dmitryzhuk99@gmail.com>
2021-08-20 17:54:42 +0300

    Add Objective-C example
    Signed-off-by: Dmitry <dmitryzhuk99@gmail.com>

---
+++ map-data-prefetcher-ios/MapDataPrefetcherApp/main.m
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011-2020 HERE Europe B.V.
+ * All rights reserved.
+ */
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
